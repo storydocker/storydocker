4548139814927fc9ac45e64d120c231a808f3a4e
Scott Nath <github@scottnath.com>
2023-05-16 11:26:36 -0400

    :arrow_up: storybook @ 7.0.12

--- package/storybook-setup/.storybook/main.js
+++ package/storybook-setup/.storybook/main.js
@@ -18,7 +18,7 @@ const config = {
   },
   async viteFinal(config, options) {
     // Ensures that the cache directory is inside the project directory
-    config.cacheDir = path.join(__dirname, '../node_modules/.vite')
+    config.cacheDir = path.join(__dirname, '../node_modules/.vite-unique-name')
     return config;
   },
 };
