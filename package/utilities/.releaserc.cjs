e53ef4976805a5326c1d82781b751cc7601d53c0
Scott Nath <github@scottnath.com>
2023-06-25 13:05:18 -0400

    :rocket: tagFormat plus test non-configured emoji

--- package/utilities/.releaserc.cjs
+++ package/utilities/.releaserc.cjs
@@ -8,6 +8,17 @@ module.exports = {
     [
       "semantic-release-gitmoji",
       {
+        tagFormat: 'storydocker-utilities@v${version}',
+        releaseRules: {
+          major: [ ':boom:' ],
+          minor: [  ],
+          patch: [
+            ':bug:',
+            ':ambulance:',
+            ':lock:'
+          ],
+          prepatch: [':sparkles:' ],
+        },
         // releaseRules: {
         //   patch: {
         //     include: [':bento:', ':arrow_up:', ':lock:'],
