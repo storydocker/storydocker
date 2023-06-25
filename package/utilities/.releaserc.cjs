19521cc1a9917cc80e9402aa99211f3e1b1e28b7
Scott Nath <github@scottnath.com>
2023-06-25 13:29:27 -0400

    :wrench: update configs with gitmojis set

--- package/utilities/.releaserc.cjs
+++ package/utilities/.releaserc.cjs
@@ -1,35 +1,24 @@
+import { gitmojis } from 'gitmojis';
+
 module.exports = {
-  "branches": ["utilities", "main"],
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-    "@semantic-release/npm",
+  branches: [
+    { name: 'main' },
+    { name: "utilities", channel: "next" }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/npm',
     [
-      "semantic-release-gitmoji",
+      'semantic-release-gitmoji',
       {
         tagFormat: 'storydocker-utilities@v${version}',
         releaseRules: {
-          major: [ ':boom:' ],
-          minor: [  ],
-          patch: [
-            ':bug:',
-            ':ambulance:',
-            ':lock:'
-          ],
-          prepatch: [':sparkles:' ],
+          major: gitmojis.filter(({ semver }) => semver === 'major').map(({ code }) => code),
+          minor: gitmojis.filter(({ semver }) => semver === 'minor').map(({ code }) => code),
+          patch: gitmojis.filter(({ semver }) => semver === 'patch').map(({ code }) => code),
         },
-        // releaseRules: {
-        //   patch: {
-        //     include: [':bento:', ':arrow_up:', ':lock:'],
-        //   },
-        //   patch: {
-        //     include: [':sparkles:'],
-        //   },
-        // },
-        // releaseNotes: {
-        //   template: fs.readFileSync(tplFile, 'utf-8'),
-        // }
       }
     ]
   ]
