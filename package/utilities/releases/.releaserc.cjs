20d62fb1de8802d6f0451e9fe2e18501b054d16a
Scott Nath <github@scottnath.com>
2023-09-06 15:59:49 -0400

    :fire: remove npm gh action; prepare for merge

--- package/utilities/releases/.releaserc.cjs
+++ package/utilities/releases/.releaserc.cjs
@@ -17,7 +17,7 @@ const releaseRules = {
 module.exports = {
   branches: [
     { name: 'main', channel: 'latest', prerelease: false },
-    { name: 'release-fixes', channel: 'release', prerelease: true },
+    { name: 'next', channel: 'next', prerelease: true },
   ],
   debug: true,
   plugins: [
